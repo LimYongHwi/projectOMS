080f5c4b1adbe16e8245f65335388564121102d2
Yong <Yong@Yong-PC>
2018-05-17 17:44:38 +0900

    aa

---
+++ Final_Project/src/VO/SearchVO.java
@@ -0,0 +1,69 @@
+package VO;
+
+import java.util.ArrayList;
+
+public class SearchVO {
+	private ArrayList<?> List;
+	private int current;
+	private int LAST;
+	private int START;
+	private int END;
+	private int SKIP;
+	private int STARTRN;
+	private int ENDRN;
+		
+	
+	public ArrayList<?> getList() {
+		return List;
+	}
+	public void setList(ArrayList<?> list) {
+		List = list;
+	}
+	public int getCurrent() {
+		return current;
+	}
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+	public int getLAST() {
+		return LAST;
+	}
+	public void setLAST(int lAST) {
+		LAST = lAST;
+	}
+	public int getSTART() {
+		return START;
+	}
+	public void setSTART(int sTART) {
+		START = sTART;
+	}
+	public int getEND() {
+		return END;
+	}
+	public void setEND(int eND) {
+		END = eND;
+	}
+	public int getSKIP() {
+		return SKIP;
+	}
+	public void setSKIP(int sKIP) {
+		SKIP = sKIP;
+	}
+	public int getSTARTRN() {
+		return STARTRN;
+	}
+	public void setSTARTRN(int sTARTRN) {
+		STARTRN = sTARTRN;
+	}
+	public int getENDRN() {
+		return ENDRN;
+	}
+	public void setENDRN(int eNDRN) {
+		ENDRN = eNDRN;
+	}
+	
+	
+	
+	
+	
+}
